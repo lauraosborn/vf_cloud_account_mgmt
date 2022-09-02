7da09e5d2c54f6d11a6664a2e2df78a27e6ffd79
lauraosborn <laura_osborn@sky.com>
2022-09-02 17:51:07 +0100

    test.py updates

--- test.py
+++ test.py
@@ -1,24 +1,24 @@
 import unittest
 from flask import current_app
-from app import create_app
+from vf_cloud_account_mgmt import create_vf_cloud_account_mgmt
 
 
 class TestWebApp(unittest.TestCase):
     def setUp(self):
-        self.app = create_app()
-        self.appctx = self.app.app_context()
-        self.appctx.push()
+        self.vf_cloud_account_mgmt = create_vf_cloud_account_mgmt()
+        self.vf_cloud_account_mgmtctx = self.vf_cloud_account_mgmt.vf_cloud_account_mgmt_context()
+        self.vf_cloud_account_mgmtctx.push()
         db.create_all()
 
     def tearDown(self):
         db.drop_all()
-        self.appctx.pop()
-        self.app = None
-        self.appctx = None
+        self.vf_cloud_account_mgmtctx.pop()
+        self.vf_cloud_account_mgmt = None
+        self.vf_cloud_account_mgmtctx = None
 
     def test_app(self):
-        assert self.app is not None
-        assert current_app == self.app
+        assert self.vf_cloud_account_mgmt is not None
+        assert current_vf_cloud_account_mgmt == self.vf_cloud_account_mgmt
 
     def test_registration_form(self):
         response = self.client.get('/new_account')
