05973b224dfa9bee3811695a331f3d19aeb3146e
lauraosborn <laura_osborn@sky.com>
2022-09-26 12:46:27 +0100

    Path updates

--- my_env/bin/activate.fish
+++ my_env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/lauraosborn/Library/Mobile Documents/com~apple~CloudDocs/QA/Level 6/Software Engineering with Agile/vf_cloud_account_mgmt/my_env"
+set -gx VIRTUAL_ENV "/Users/lauraosborn/Library/Mobile Documents/com~apple~CloudDocs/QA/Level 6/Software Engineering with DevOps/vf_cloud_account_mgmt/my_env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
